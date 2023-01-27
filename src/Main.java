b5f663006a982707b4e55629ad2916c466fa02bd
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-27 08:44:28 +0100

    Squash colaborador

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        //modificacion colaborador 1
+        //modificacion colaborador 2
+        //modificacion colaborador 3
     }
 }
