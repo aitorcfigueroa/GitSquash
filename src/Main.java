458ef54f3a09831dabe6e3fe6a2950879cbbbc44
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-27 08:50:39 +0100

    Squash lider

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //Un poco de código.
+        //Un poco más de código.
+        //último trozo de código.
         //modificacion colaborador 1
         //modificacion colaborador 2
         //modificacion colaborador 3
